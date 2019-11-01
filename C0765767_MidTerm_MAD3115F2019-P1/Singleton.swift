6994d6e52090c22766406ae4cde5d31f10aead9c
Ankita Jain <ankita.jain12342@gmail.com>
2019-11-01 00:45:31 -0400

    created function to add new customer

--- C0765767_MidTerm_MAD3115F2019-P1/Singleton.swift
+++ C0765767_MidTerm_MAD3115F2019-P1/Singleton.swift
@@ -19,5 +19,10 @@ class Singleton:NSObject
         return s1
     }
     
+    func addNewCustomer(FirstName:String,LastName:String,EmailId:String)
+    {
+        let cust1=custDict.count+1
+        let add=Customer(cId: cust1, cFname: FirstName, cLname: LastName, cEmail: EmailId)
     
+    }
 }
