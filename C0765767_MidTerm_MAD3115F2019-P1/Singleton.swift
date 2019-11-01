3ed650208c07b869193d9ffcb1b78593bda1d394
Ankita Jain <ankita.jain12342@gmail.com>
2019-11-01 00:52:01 -0400

    function to display the name of the customer

--- C0765767_MidTerm_MAD3115F2019-P1/Singleton.swift
+++ C0765767_MidTerm_MAD3115F2019-P1/Singleton.swift
@@ -38,6 +38,12 @@ class Singleton:NSObject
         let c2=Customer(cId: 102, cFname: "Honey", cLname: "Singh", cEmail: "honey@123")
     }
     
-   
+   func display()
+      {
+          for i in custDict.values
+          {
+              print(i.fullName)
+          }
+      }
     
 }
