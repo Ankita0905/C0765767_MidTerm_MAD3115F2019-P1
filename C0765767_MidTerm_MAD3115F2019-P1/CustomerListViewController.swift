783ddebf101da47906ee0d14713b684406054541
Ankita Jain <ankita.jain12342@gmail.com>
2019-11-01 01:03:54 -0400

    updating customer view controller

--- C0765767_MidTerm_MAD3115F2019-P1/CustomerListViewController.swift
+++ C0765767_MidTerm_MAD3115F2019-P1/CustomerListViewController.swift
@@ -8,24 +8,33 @@
 
 import UIKit
 
-class CustomerListViewController: UIViewController {
+class CustomerListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
-
+    @IBOutlet weak var customerListTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        customerListTable.delegate=self
+        customerListTable.dataSource=self
       navigationItem.hidesBackButton=true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 
+    
+    
+
+    
 }
