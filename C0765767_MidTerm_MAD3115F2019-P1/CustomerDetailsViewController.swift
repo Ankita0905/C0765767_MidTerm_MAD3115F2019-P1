efe97359833c9ba82249aef7c5bfe119be382908
Ankita Jain <ankita.jain12342@gmail.com>
2019-11-08 00:52:13 -0500

    changes in customer details vc

--- C0765767_MidTerm_MAD3115F2019-P1/CustomerDetailsViewController.swift
+++ C0765767_MidTerm_MAD3115F2019-P1/CustomerDetailsViewController.swift
@@ -8,16 +8,39 @@
 
 import UIKit
 
-class CustomerDetailsViewController: UIViewController {
+class CustomerDetailsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
+{
     var cutdetails:Customer?=nil
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return cutdetails!.billDictionary.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let x = cutdetails.returnCustObject(custID: Int(indexPath.row+1))
+        let Cell=tableView.dequeueReusableCell(withIdentifier: "Mycell",for:indexPath)
+        
+        return Cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    
     @IBOutlet weak var lblDetails: UILabel!
     @IBOutlet weak var lbldetails2: UILabel!
+    
+    @IBOutlet weak var tblBillView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         lblDetails.text = cutdetails?.fullName
         lbldetails2.text = cutdetails?.customerEmail
         // Do any additional setup after loading the view.
     }
+    
     
 
     /*
